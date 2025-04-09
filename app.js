b2f0aa899cafac0be6ff7f1352c355c95ce8027f
adrian alvi yendri <adrianalviyendri@gmail.com>
2025-04-09 21:33:49 +0700

    feat: Integrate Socket.IO for real-time communication and update dependencies

--- app.js
+++ app.js
@@ -5,16 +5,36 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const router = require("./routes");
 const cors = require("cors");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
 const app = express();
-const PORT = 3000
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+const PORT = 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 app.use(cors());
 
 app.use("/", router);
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+
+  socket.on("ping", (data) => {
+    console.log("Ping received from client", data);
+    socket.emit("pong");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server can be access in http://localhost:${PORT}`);
-})
+});
